bd41af1665aa3d0b9af9c102c46931d908b991b0
Teresa Godat <teresagodat@yahoo.com>
2017-06-14 05:27:05 -0700

    added more content and made responsive

--- js/script.js
+++ js/script.js
@@ -1,29 +1,3 @@
-//var bars = '<div id="bars"><div class="n"></div><div class="n"></div><div class="m"></div><div class="m"></div><div class="m"></div><div class="c"></div></div>' $('#container').prepend(bars).css({paddingTop: 0}); $('body').prepend('<div id="shadow" />')   
-//
-//
-//$('#blog').bind('toggle', function()    
-//{   
-//    var $blog = $(this);   
-//    if ($blog.is(':visible')) $blog.trigger('hide');   
-//    else $blog.trigger('show')   
-//})   
-//    .bind('show', function()   
-//    {   
-//    $(this).slideDown('show');   
-//})   
-//    .bind('hide', function()    
-//    {   
-//    $(this).slideUp('hide');   
-//});   
-//$('#blogbutton').removeAttr('href').   
-//click(function()    
-//{   
-//    $('#blog').trigger('toggle');   
-//});   
-//
-//
-//
-
 jQuery(document).ready(function() {
 var offset = 220;
 var duration = 500;
